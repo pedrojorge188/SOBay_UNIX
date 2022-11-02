2554fdfff0a9877c2ab5852b4f8834f6861a768c
Pedro Jorge <pedrorodriguesjorge03@gmail.com>
2022-11-02 18:27:27 +0000

    updates
    -> backend structs
    -> frontend setup logs

--- backend.h
+++ backend.h
@@ -1,8 +1,30 @@
 #ifndef BACKEND_H
 #define BACKEND_H
 
+#define ROOTNAME "admin";
 
 /*Setup all data structs*/
+typedef struct client{
+    int user_pid;
+    char **name;
+    int password;
+    int *id_own_items;
+    int money;
+    bool connection;
+    client *pnext;
+}client;
+
+typedef struct items{
+    int id;
+    char **name;
+    char **category;
+    int buy_now_price;
+    int main_price;
+    int id_owner;
+    bool sell_state;
+    int time_left;
+    items *pnext;
+}items;
 
 
 #endif
--- frontend.h
+++ frontend.h
@@ -1,8 +1,16 @@
 #ifndef FRONTEND_H
 #define FRONTEND_H
 
+#define HEARTBEAT_TIMER 20;
+#define LOGIN_ERROR "THE DATA ENTERED IS NOT CORRECT!";
+#define KICK_LOG "YOU HAVE BEEN KICKED FROM YOUR SESSION!";
 
 /*Setup all data structs*/
-
+typedef struct user{
+    char **name;
+    int money;
+    int id_items;
+}user;
 
 #endif
+
