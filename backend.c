ad5e097dc086e8a300c73b89aa2ffbf4bac6f911
Pedro Jorge <pedrorodriguesjorge03@gmail.com>
2022-11-02 15:36:53 +0000

    Files creation

---
+++ backend.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+
+#include <backend.h>
+
+
+int main(int argc, char **argv) {
+
+    /*CODE*/
+
+    return 0;
+}
---
+++ frontend.h
@@ -0,0 +1,8 @@
+#ifndef FRONTEND_H
+#define FRONTEND_H
+
+
+/*Setup all data structs*/
+
+
+#endif
